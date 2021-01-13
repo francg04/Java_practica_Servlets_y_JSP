90a218493515825be94c62a5e7f4addafb32632c
francg04 <francg04@hotmail.com>
2021-01-13 17:53:15 -0300

    [prueba]Actualizamos inserta_producto.jsp y Clase ControladorProductos

--- WebContent/inserta_producto.jsp
+++ WebContent/inserta_producto.jsp
@@ -8,7 +8,8 @@
 </head>
 <body>
 <h1>Insertar Registro</h1>
-<form action="">
+<form action="ControladorProductos" method="get">
+<input type="hidden" name="instruccion" value="insertarBBDD">
 	<table>
 		<tr>
 			<td><label for="txtCodigo">Codigo Articulo</label></td><td><input type="text" name="txtCodigo"></td>
@@ -27,6 +28,21 @@
 		</tr>
 		<tr>
 			<td><label>Importado</label></td><td><label for="Si">Si</label> <input type="radio" id="Si"name="rbtImportado" value="true"> <label for="No">No</label> <input type="radio" id="No" name="rbtImportado" value="false"></td>
+		</tr>
+		<tr>
+			<td><label for="txtOrigen">Origen</label></td><td><select name="txtOrigen">
+			<option value="espania">ESPAÑA</option>
+			<option value="italia">ITALIA</option>
+			<option value="marruecos">MARRUECOS</option>
+			<option value="usa">USA</option>
+			<option value="francia">FRANCIA</option>
+			<option value="japon">JAPÓN</option>
+			<option value="china">CHINA</option>
+			<option value="suecia">SUECIA</option>
+			<option value="turquia">TURQUÍA</option>
+			<option value="taiwan">TAIWÁN</option>
+			
+			</select></td>
 		</tr>
 		<tr>
 			<td><input type="submit" name="enviar" value="Actualizar"></td><td><input type="reset" name="borrar" value="Refrescar"></td>
--- src/com/pildorasinformaticas/producto/ControladorProductos.java
+++ src/com/pildorasinformaticas/producto/ControladorProductos.java
@@ -20,6 +20,7 @@ public class ControladorProductos extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 	private ModeloProductos modelo_productos;
+	private List<Producto> productos;
 	
 	@Resource(name="jdbc/Productos")
 	private DataSource origen_Datos;
@@ -39,28 +40,77 @@ public class ControladorProductos extends HttpServlet
 			throw new ServletException(e);
 		}
 	}
-
+	/*
+	 * ESPAÑA
+	ITALIA
+	MARRUECOS
+	USA
+	FRANCIA
+	JAPÓN
+	CHINA
+	SUECIA
+	TURQUÍA
+	TAIWÁN
+	 */
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		//Obtener la lista de productos desde el modelo
-		List<Producto> productos;
+		// Leer el parametro del formulario
+		String parametro = request.getParameter("instruccion");
+		
+		//si no se envia el parametro, listar productos
+		if(parametro==null) {
+			parametro = "listar";
+		}
+		// Redirigir el flujo de ejecucion al metodo adecuado
+		switch(parametro) {
+		case "listar":{
+			//Obtener la lista de productos desde el modelo
+			obtenerProductos(request, response);
+		}break;
+		case "insertarBBDD":{
+			agregarProducto(request, response);
+		}break;
+		default:{
+			obtenerProductos(request, response);
+		}
+		}
+	}
+	private void agregarProducto(HttpServletRequest request, HttpServletResponse response) {
+		//Leer la informacion del producto que viene del formulario
+		String codigo = request.getParameter("txtCodigo");
+		String seccion = request.getParameter("txtSeccion");
+		String articulo = request.getParameter("txtArticulo");
+		String fecha = request.getParameter("txtFecha");
+		String precio = request.getParameter("txtPrecio");
+		String importado = request.getParameter("rbtImportado");
+		
+			
+		//Crear un objeto de tipo Producto
+		
+		//Enviar el objeto al modelo y despues insertar el objeto Producto en la BBDD
+		
+		//Volver al listado de Productos
+	}
+
+
+	private void obtenerProductos(HttpServletRequest request, HttpServletResponse response)
+	{
 		try {
 			productos = modelo_productos.getProductos();
-		// Agregar lista de productos al request
+			// Agregar lista de productos al request
 			request.setAttribute("lista_productos", productos);
-		//Enviar ese request a la pagina JSP
+			//Enviar ese request a la pagina JSP
 			
 			RequestDispatcher mensajero = request.getRequestDispatcher("/ListaProductos.jsp");
-			
+					
 			mensajero.forward(request, response);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
