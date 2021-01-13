be6424a70e5440b901a12ac11239d1eed552dd81
francg04 <francg04@hotmail.com>
2021-01-13 17:44:24 -0300

    [prueba 3]

---
+++ src/com/pildorasinformaticas/producto/ModeloProductos.java
@@ -0,0 +1,59 @@
+package com.pildorasinformaticas.producto;
+
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+
+import javax.sql.DataSource;
+
+public class ModeloProductos
+{
+	private DataSource origenDatos;
+	
+	public ModeloProductos(DataSource orgienDatos)
+	{
+		this.origenDatos = orgienDatos;
+	}
+	
+	public List<Producto> getProductos() throws Exception{
+		List<Producto> productos = new ArrayList<>();
+		
+		Connection conexion = null;
+		Statement consulta = null;
+		ResultSet resultado = null;
+		
+		//------------establecer la conexion--------------
+		conexion=origenDatos.getConnection();
+		
+		//------------crear sentencia sql y statement-----
+		String consultaSQL = "SELECT * FROM PRODUCTOS";
+		consulta = conexion.createStatement();
+		
+		//------------ejecutar sql------------------------
+		resultado = consulta.executeQuery(consultaSQL);
+		//------------recorrer el ResultSet obtenido------
+		while(resultado.next()) {
+			/*
+			String cod_art = resultado.getString("CÓDIGOARTÍCULO");
+			String seccion = resultado.getString("SECCIÓN");
+			String nom_art = resultado.getString("NOMBREARTÍCULO");
+			double precio = resultado.getDouble("PRECIO");
+			Date fecha = resultado.getDate("FECHA");
+			boolean importado = resultado.getBoolean("IMPORTADO");
+			String pais_orig = resultado.getString("PAÍSDEORIGEN");
+			
+			Producto producto = new Producto(cod_art, seccion, nom_art, precio, fecha, importado, pais_orig);
+			
+			productos.add(producto);
+			*/
+			productos.add(new Producto(resultado.getString("CÓDIGOARTÍCULO"),
+									   resultado.getString("SECCIÓN"),
+									   resultado.getString("NOMBREARTÍCULO"),
+									   resultado.getDouble("PRECIO"),
+									   resultado.getDate("FECHA"),
+									   resultado.getBoolean("IMPORTADO"),
+									   resultado.getString("PAÍSDEORIGEN")));
+		}
+		return productos;
+	}
+}
---
+++ src/com/pildorasinformaticas/producto/Producto.java
@@ -0,0 +1,98 @@
+package com.pildorasinformaticas.producto;
+
+import java.util.Date;
+
+public class Producto
+{
+	private String codigoArt;
+	private String seccion;
+	private String nombreArt;
+	private double precio;
+	private Date fecha;
+	private boolean importado;
+	private String paisOrigen;
+	
+	public Producto(String codigoArt, String seccion, String nombreArt, double precio, Date fecha, boolean importado,
+			String paisOrigen) {
+		super();
+		this.codigoArt = codigoArt;
+		this.seccion = seccion;
+		this.nombreArt = nombreArt;
+		this.precio = precio;
+		this.fecha = fecha;
+		this.importado = importado;
+		this.paisOrigen = paisOrigen;
+	}
+
+	public Producto(String seccion, String nombreArt, double precio, Date fecha, boolean importado, String paisOrigen) {
+		super();
+		this.seccion = seccion;
+		this.nombreArt = nombreArt;
+		this.precio = precio;
+		this.fecha = fecha;
+		this.importado = importado;
+		this.paisOrigen = paisOrigen;
+	}
+	
+	@Override
+	public String toString() {
+		return "Producto [codigoArt=" + codigoArt + ", seccion=" + seccion + ", nombreArt=" + nombreArt + ", precio="
+				+ precio + ", fecha=" + fecha + ", importado=" + importado + ", paisOrigen=" + paisOrigen + "]";
+	}
+
+	public String getCodigoArt() {
+		return codigoArt;
+	}
+
+	public void setCodigoArt(String codigoArt) {
+		this.codigoArt = codigoArt;
+	}
+
+	public String getSeccion() {
+		return seccion;
+	}
+
+	public void setSeccion(String seccion) {
+		this.seccion = seccion;
+	}
+
+	public String getNombreArt() {
+		return nombreArt;
+	}
+
+	public void setNombreArt(String nombreArt) {
+		this.nombreArt = nombreArt;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public boolean isImportado() {
+		return importado;
+	}
+
+	public void setImportado(boolean importado) {
+		this.importado = importado;
+	}
+
+	public String getPaisOrigen() {
+		return paisOrigen;
+	}
+
+	public void setPaisOrigen(String paisOrigen) {
+		this.paisOrigen = paisOrigen;
+	}
+}
